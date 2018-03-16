cd3eb4102a89f55b2b1598fcf5899976d5a26055
xemuliam <xemuliam@gmail.com>
2018-03-16 13:47:45 +0200

    added API for lambdas

--- node-example.js
+++ node-example.js
@@ -1,5 +1,41 @@
 exports.handler = (event, context, callback) => {
+	var output = "";
 
-    callback(null, "Hey " + event.name + "!");
+    if (event.requestContext) {
 
+		var payload = "";
+
+		params = event.pathParameters;
+
+		if (event.body) {
+			payload = event.body;
+            const body = JSON.parse(payload);
+            for(key in body) {
+                params[key] = body[key];
+            }
+        }
+
+		switch(event.requestContext.httpMethod) {
+
+			case 'GET':
+				output = "Hey " + params.name + "!\n";
+				break;
+					
+			case 'POST':
+				output = "Hey " + params.name + "!\n"
+						+ "\n"
+						+ "Your payload:\n"
+						+ payload + "\n";
+				break;
+		}
+		
+		callback(null, {
+			statusCode: 200,
+			body: output
+		})
+
+	} else {
+		output = "Hey " + event.name + "!";
+		callback(null, output);
+	}
 }
--- python-example.py
+++ python-example.py
@@ -1,3 +1,31 @@
-def handler(event, context):
+import json
 
-    return "Hey " + event['name'] + "!"
+def handler(event, context):
+	output = ""
+
+	if 'requestContext' in event:
+
+		payload = "";
+
+		params = event['pathParameters']
+		if event['body']:
+			payload = event['body']
+			body = json.loads(event['body'])
+			for key in body:
+				params[key] = body[key]
+
+		output = {
+			'GET': "Hey " + params['name'] + "!\n",
+			'POST': "Hey " + params['name'] + "!\n" \
+					+ "\n" \
+					+ "Your payload:\n" \
+					+ payload + "\n"
+		}[event['requestContext']['httpMethod']]
+
+		return { \
+			"statusCode": 200, \
+			"body": output \
+			}
+	else:
+		output = "Hey " + event['name'] + "!"
+		return output
